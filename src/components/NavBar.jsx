356836a83471ab1dd5aa1acf73789c1f38c1955e
Khaing Maw <khaingmaw@Khaings-MacBook-Air.local>
2023-06-24 14:11:24 +0900

    Responsive with media query to Top

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,7 +4,7 @@ const NavBar = () => {
 
   return (
     <>
-      <nav className="w-full bg-gray-100 text-slate-700 p-[10px] fixed top-0 z-10" >
+      <nav className="hidden md:block w-full bg-gray-100 text-slate-700 p-[10px] fixed top-0 z-10" >
         <ul className="w-[70%] mx-auto flex justify-between">
           <li className="">
              <a href='' className=''>
